e6693b60058147d7081e6eb1082c701d1bbed307
Damir <dense.neer@gmail.com>
2023-03-31 19:04:36 +0600

    Problem 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Input problem");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        switch (input){
+            case 1:
+                Problem1 pr1 = new Problem1();
+                System.out.println("Input len of array");
+                int n = scanner.nextInt();
+                int[] arr = new int[n];
+                System.out.println("Input array by space");
+                for(int i = 0; i < n; i++){
+                    arr[i] = scanner.nextInt();
+                }
+                System.out.println("min - " + pr1.minimumNum(arr, n));
+        }
     }
 }
---
+++ src/Problem1.java
@@ -0,0 +1,14 @@
+public class Problem1 {
+    public int minimumNum(int[] arr, int n){
+        if(n == 0){
+            return arr[0];
+        }
+        else{
+            int min = minimumNum(arr,n - 1);
+            if (min <= arr[n - 1])
+                return min;
+            else
+                return arr[n-1];
+        }
+    }
+}
